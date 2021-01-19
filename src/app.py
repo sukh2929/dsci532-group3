f6a0c2903379c774e085727e1268e8bd13be3b82
Tran Doan Khanh Vu <tdkhanhvu@gmail.com>
2021-01-18 22:11:42 -0800

    Filter by continent and country

--- src/app.py
+++ src/app.py
@@ -9,40 +9,75 @@ import pandas as pd
 from pathlib import Path
 from datetime import datetime
 
+def calculate_countries_monthwise(df):
+    countries_daywise_df = df.copy()
+    countries_daywise_df['Month'] = countries_daywise_df['Date'].apply(lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%b"))
+    countries_daywise_df = countries_daywise_df.drop(['Date'], axis=1).groupby(['Month', 'Country/Region', 'Population']).agg(agg_steps).reset_index()
+
+    return countries_daywise_df
+
+def calculate_continent_daywise(countries_daywise_df):
+    return calculate_continent_statistics(countries_daywise_df, 'Date')
+
+def calculate_continent_monthwise(countries_monthwise_df):
+    return calculate_continent_statistics(countries_monthwise_df, 'Month')
+
+def calculate_continent_statistics(countries_df, group_col):
+    all_df = countries_df.drop(['Country/Region', 'Population'], axis=1).groupby([group_col, 'WHO Region']).agg('mean').reset_index()
+    all_df['Country/Region'] = 'All'
+    all_df['Population'] = population_data['Population'].sum()
+
+    return all_df
+
+def calculate_world_daywise(countries_daywise_df):
+    return calculate_world_statistics(countries_daywise_df, 'Date')
+
+def calculate_world_monthwise(countries_monthwise_df):
+    return calculate_world_statistics(countries_monthwise_df, 'Month')
+
+def calculate_world_statistics(countries_df, group_col):
+    world_df = countries_df.drop(['Country/Region', 'Population'], axis=1).groupby(group_col).agg('mean').reset_index()
+    world_df['Country/Region'] = 'All'
+    world_df['WHO Region'] = 'All'
+    world_df['Population'] = population_data['Population'].sum()
+
+    return world_df
+    
 alt.data_transformers.disable_max_rows()
+
+month_order = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+prev_vals = {'country': None, 'continent': None, 'date': None}
+
 # print(Path.cwd())
 month_data = pd.read_csv(".\\data\\raw\\full_grouped.csv")
 
 population_data = pd.read_csv(".\\data\\raw\\worldometer_data.csv",
     usecols = ['Country/Region','Population'])
 
-daywise_df = month_data.merge(population_data, how = 'left', on = 'Country/Region')
-daywise_df['Month'] = daywise_df['Date'].apply(lambda x: datetime.strftime(datetime.strptime(x, "%Y-%m-%d"), "%b"))
+df = month_data.merge(population_data, how = 'left', on = 'Country/Region')
 
 metrics = ['Confirmed',	'Deaths', 'Recovered', 'Active', 'New cases', 'New deaths', 'New recovered']
-keep_cols = ['WHO Region']
 
 agg_steps = {metric: 'mean' for metric in metrics}
-for col in keep_cols:
-    agg_steps[col] = 'first'
+agg_steps['WHO Region'] = 'first' # to keep this column in aggregate
 
-df = daywise_df.drop(['Date'], axis=1).groupby(['Month', 'Country/Region', 'Population']).agg(agg_steps).reset_index()
+countries_daywise_df = df
+countries_monthwise_df = calculate_countries_monthwise(df)
 
-for col in keep_cols:
-    del agg_steps[col]
+continents_daywise_df = calculate_continent_daywise(countries_daywise_df)
+continents_monthwise_df = calculate_continent_monthwise(countries_monthwise_df)
 
-all_df = daywise_df.drop(['Country/Region', 'Population', 'Date'], axis=1).groupby(['Month']).agg(agg_steps).reset_index()
-all_df['Country/Region'] = 'All'
-all_df['WHO Region'] = 'All'
-all_df['Population'] = population_data['Population'].sum()
+world_daywise_df = calculate_world_daywise(countries_daywise_df)
+world_monthwise_df = calculate_world_monthwise(countries_monthwise_df)
 
-month_order = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+# df = pd.concat([all_df, df], axis=0)
+# df = df.sort_values(by=['Month', 'Country/Region'], ascending=[month_order, True])
 
-df = pd.concat([all_df, df], axis=0)
-df = df.sort_values(by=['Month', 'Country/Region'], ascending=[month_order, True])
-
-countries = ['All'] + list(set(daywise_df['Country/Region'].tolist()))
+countries = ['All'] + list(set(df['Country/Region'].tolist()))
+continents = ['All'] + list(set(df['WHO Region'].tolist()))
 countries.sort()
+continents.sort()
 
 # Setup app and layout/frontend
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -50,27 +85,55 @@ app.layout = dbc.Container([
     html.H1('COVID-19'),
     dbc.Row([
         dbc.Col([
-            dcc.Dropdown(
-                id='country_filter',
-                value='All',  # REQUIRED to show the plot on the first page load
-                options=[{'label': country, 'value': country} for country in countries])],
-            md=4),
+            dbc.Row([
+                dbc.Col([
+                    html.Label([
+                        'Continent Selection',
+                        dcc.Dropdown(
+                            id='continent_filter',
+                            value='All',  # REQUIRED to show the plot on the first page load
+                            options=[{'label': continent, 'value': continent} for continent in continents])
+                    ])]),
+                    dbc.Col([
+                        html.Label([
+                            'Country Selection',
+                            dcc.Dropdown(
+                                id='country_filter',
+                                value='All',  # REQUIRED to show the plot on the first page load
+                                options=[{'label': country, 'value': country} for country in countries])
+                        ])])]),], md=4),
         dbc.Col(
             html.Iframe(
                 id='line_totalcases',
                 style={'border-width': '0', 'width': '100vw', 'height': '100vh'}), md=8)])])
+
 # Set up callbacks/backend
 @app.callback(
     Output('line_totalcases', 'srcDoc'),
-    Input('country_filter', 'value'))
-def plot_altair(country):
-    data = df[df['Country/Region'] == country]
+    Input('country_filter', 'value'),
+    Input('continent_filter', 'value'))
+def filter_plot(country, continent):
+    if prev_vals['continent'] == None or prev_vals['continent'] != continent: #continent is changed
+        prev_vals['continent'] = continent
+        if continent == 'All':
+            data = world_monthwise_df
+        else:
+            data = continents_monthwise_df[continents_monthwise_df['WHO Region'] == continent]
+    elif prev_vals['country'] == None or prev_vals['country'] != country: #country is changed
+        prev_vals['country'] = country
+        if country == 'All':
+            data = world_monthwise_df
+        else:
+            data = countries_monthwise_df[countries_monthwise_df['Country/Region'] == country]
 
+    return plot(data)
+
+def plot(data):
     chart = alt.Chart(data).mark_line().encode(
         x=alt.X('Month:N', sort=month_order),
         y='Confirmed:Q',
-        tooltip='Month').interactive()
-    return chart.to_html()
+        tooltip='Confirmed:Q')
+    return (chart + chart.mark_point()).interactive().to_html()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
